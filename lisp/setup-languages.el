08cedcd8e42493f9eba9e1ac08256e8d2cc0e536
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2022-05-11 12:31:00 +0200

    Add support for meson, python, yaml, and docker. Ruby Auto-insert

--- lisp/setup-languages.el
+++ lisp/setup-languages.el
@@ -14,6 +14,15 @@
 (use-package cc-mode
   :config (setq-default c-basic-offset c-tab-width))
 
+;; meson build system
+(use-package meson-mode
+  :ensure t)
+
+;; python
+(setq python-tab-width 4)
+(use-package python
+  :config (setq-default python-indent-offset python-tab-width))
+
 ;; haskell
 (use-package haskell-mode
   :ensure t)
@@ -22,5 +31,18 @@
   :ensure t
   :hook ((haskell-mode . lsp-deferred)
 		 (haskell-mode . interactive-haskell-mode)))
+
+;; yaml
+(use-package yaml-mode
+  :ensure t)
+
+;; docker
+(use-package dockerfile-mode
+  :ensure t)
+
+(auto-insert-mode t)
+(setq auto-insert-query nil
+	  auto-insert-directory "~/.emacs.d/auto-insert/"
+	  auto-insert-alist '((ruby-mode . "ruby.el")))
 
 (provide 'setup-languages)
