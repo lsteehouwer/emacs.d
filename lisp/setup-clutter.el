986c471ad013e90b66116edc9dd0bb20b13c1f99
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2022-05-19 11:41:10 +0200

    Reduce emacs littering

---
+++ lisp/setup-clutter.el
@@ -0,0 +1,7 @@
+;; move backup files to tmp directory
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+;; move autosave files to tmp directory
+(make-directory (expand-file-name "tmp/autosave" user-emacs-directory) t)
+(setq auto-save-list-file-prefix (expand-file-name "tmp/autosave/" user-emacs-directory)
+	  auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/autosave" user-emacs-directory) t)))
