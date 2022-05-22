3c3080fab38f0a076e3ae4221f6367c603522dbe
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2022-05-23 00:08:13 +0200

    Just make snippets globally available. Slower, but easier.

--- lisp/setup-snippets.el
+++ lisp/setup-snippets.el
@@ -1,15 +1,6 @@
 (use-package yasnippet
   :ensure t
-  :commands (yas-minor-mode-on
-             yas-expand
-             yas-expand-snippet
-             yas-lookup-snippet
-             yas-insert-snippet
-             yas-new-snippet
-             yas-visit-snippet-file
-             yas-active-extra-mode
-             yas-deactive-extra-mode
-             yas-maybe-expand-abbrev-key-filter))
+  :config (yas-global-mode))
 
 (use-package yasnippet-snippets
   :after yasnippet
