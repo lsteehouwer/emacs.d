7e4bcca848d6a88bcafa95ce1eaff47f36f6d547
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2023-01-08 12:42:44 +0100

    Make dired a bit nicer. Remove ranger

--- lisp/setup-dired.el
+++ lisp/setup-dired.el
@@ -1,11 +1,13 @@
 (use-package dired
   :ensure nil
-  :commands (dired dired-jump dired-other-tab dired-other-window dired-other-frame dired-jump-other-window)
+  :commands (dired dired-jump dired-other-tab dired-other-window
+                   dired-other-frame dired-jump-other-window)
   :config
   (setq dired-auto-revert-buffer t
         dired-listing-switches "-la --group-directories-first"))
 
-(use-package ranger)
+(use-package diredfl
+  :hook (dired-mode . diredfl-mode))
 
 (provide 'setup-dired)
 ;;; setup-dired.el ends here
