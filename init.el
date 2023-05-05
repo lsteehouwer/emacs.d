393b8bf0c3129ca079eda77fd64a0764df2ba937
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2023-05-05 14:13:13 +0200

    Regenerate config.el if config.org is a newer file

--- init.el
+++ init.el
@@ -1,17 +1,24 @@
 ;;; Package -- Summary
 
 ;;; Commentary:
-;; Init file.  It generates a configuration from config.org if it does not exist
+;; Init file. It generates a configuration from config.org if it does not exist
 ;; and then loads it
 
 ;;; Code:
+(defun ls/tangle-load (file)
+  "Tangle org file FILE and load it."
+  (require 'org)
+  (org-babel-load-file file))
+
 (let ((file-name-handler-alist nil)
       (config-file (expand-file-name "config.org" user-emacs-directory))
       (tangled-file (expand-file-name "config.el" user-emacs-directory)))
 
-  (if (file-exists-p tangled-file)
-      (load-file tangled-file)
-    (require 'org)
-    (org-babel-load-file config-file)))
-
+  (if (not (file-exists-p tangled-file))
+      (ls/tangle-load config-file)
+    (let ((mod-time-config  (nth 5 (file-attributes config-file)))
+          (mod-time-tangled (nth 5 (file-attributes tangled-file))))
+      (if (time-less-p mod-time-tangled mod-time-config)
+          (ls/tangle-load config-file)
+        (load-file tangled-file)))))
 ;;; init.el ends here
